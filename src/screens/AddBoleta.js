d66c4f4273e043b7a5924cca39d320670708f377
ASchwartzman <aleschwartzman22@gmail.com>
2018-10-28 13:56:31 -0300

    Salvar Boletas / Picker Header customizado

--- src/screens/AddBoleta.js
+++ src/screens/AddBoleta.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
-import {StyleSheet, View} from 'react-native'
-import {Form, Picker, Text, Textarea, Input, Item, Label, Icon, DatePicker, Container, Body} from 'native-base'
+import {StyleSheet, View, Alert} from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import {Title, Form, Picker, Button, Textarea, Input, Item, Label, Icon, DatePicker, Container, Header, TabHeading} from 'native-base'
 import moment from 'moment'
 import 'moment/locale/pt-br'
 import AddBoletaHeader from '../components/AddBoletaHeader'
+import BoletaItem from '../components/BoletaItem';
+// import PickerAtivo from '../components/PickerAtivo'
 
 export default class AddBoleta extends Component {
     static navigationOptions = {
@@ -14,7 +17,7 @@ export default class AddBoleta extends Component {
     constructor(props) {
         super(props)
         this.state = {
-          resultadosPossiveis:['Brasil','Argentina','França'],  
+          resultadosPossiveis:this.props.navigation.getParam('resultadosPossiveis'),  
           ativo: null,
           operation:null,
           lote: null,
@@ -25,6 +28,30 @@ export default class AddBoleta extends Component {
           comments: '',
         }
       }
+    onPressSave = () => {
+        // if(this.state.ativo == null || this.state.operation === null || 
+        //     this.state.lote === null || this.state.tradePrice === null ||
+        //     this.state.contraparte === null) {
+            
+        //     Alert.alert('Campos Incompletos','Preencha os campos em laranja')
+        //     return
+        // }
+
+        let newBoleta ={
+            id: Math.random(),
+            checked: false,
+            ativo: this.state.ativo,
+            operation: this.state.operation,
+            lote: this.state.lote,
+            tradePrice: this.state.tradePrice,
+            contraparte: this.state.contraparte,
+            tradeDate: this.state.tradeDate,
+            settleDate: this.state.settleDate,
+            comments: this.state.comments
+        }
+        const flagNewBoleta = true
+        this.props.navigation.navigate('WalletScreen', {flagNewBoleta, newBoleta})
+    }
 
     onAtivoChange = (value) => {
         this.setState({ ativo: value })
@@ -74,127 +101,133 @@ export default class AddBoleta extends Component {
 
         return (
             <Container>      
-                <AddBoletaHeader onSave={()=>alert('salvo')} onCancel={()=> this.cancelarBoleta()}/>                                 
-                <Form>
-                    <Item inlineLabel style={styles.item}>
-                        <Label style={styles.label}>Data</Label>
-                        <DatePicker
-                            defaultDate={new Date()}
-                            minimumDate={new Date(2000, 1, 1)}
-                            maximumDate={new Date(2100, 12, 31)}
-                            locale={"en"}
-                            timeZoneOffsetInMinutes={undefined}
-                            modalTransparent={true}
-                            animationType={'slide'}
-                            androidMode={"default"}
-                            placeHolderText="Selecione a data"
-                            textStyle={{ color: "grey" }}
-                            placeHolderTextStyle={{ color: "#d3d3d3" }}
-                            onDateChange={this.onTradeDateChange.bind(this)}
-                        />
-                    </Item>
-                    
-                    <Item inlineLabel style={styles.item}>
-                        <Label style={styles.label}>Ativo</Label>
-                        <Picker 
-                            placeholder='Selecione o ativo'
-                            iosIcon={<Icon name="ios-arrow-down-outline" />}
-                            mode='dropdown'
-                            style={{ }}
-                            placeholderStyle={{color:'#E65100'}}
-                            selectedValue={this.state.ativo}
-                            onValueChange={this.onAtivoChange.bind(this)}>
+                <AddBoletaHeader onSave={()=>this.onPressSave()} onCancel={()=> this.cancelarBoleta()}/>                                 
+                <KeyboardAwareScrollView>
+                    <Form>
+                        <Item inlineLabel style={styles.item}>
+                            <Label style={styles.label}>Data</Label>
+                            <DatePicker
+                                defaultDate={new Date()}
+                                minimumDate={new Date(2000, 1, 1)}
+                                maximumDate={new Date(2100, 12, 31)}
+                                locale={"en"}
+                                timeZoneOffsetInMinutes={undefined}
+                                modalTransparent={true}
+                                animationType={'slide'}
+                                androidMode={"default"}
+                                placeHolderText={moment(this.state.date).locale('pt-br').format('DD/MM/YYYY')}
+                                value={this.state.tradeDate}
+                                textStyle={{ color: "grey" }}
+                                placeHolderTextStyle={{ color: "#d3d3d3" }}
+                                onDateChange={this.onTradeDateChange.bind(this)}
+                            />
+                        </Item>
+                        
+                        <Item inlineLabel style={styles.item}>
+                            <Label style={styles.label}>Ativo</Label>
+                            <Picker
+                                iosHeader='Ativos'
+                                placeholder='Selecione o ativo'
+                                iosIcon={<Icon name="ios-arrow-down-outline" />}
+                                mode='dropdown'
+                                style={{ }}
+                                placeholderStyle={{color:'#E65100'}}
+                                selectedValue={this.state.ativo}
+                                onValueChange={this.onAtivoChange.bind(this)}>
 
-                            {resultadosPossiveis}
+                                {resultadosPossiveis}
 
-                        </Picker>
-                    </Item>
+                            </Picker>
+                            {/* <PickerAtivo resultadosPossiveis={resultadosPossiveis} /> */}
+                        </Item>
 
-                    <Item inlineLabel style={styles.item}>
-                        <Label style={styles.label}>Operação</Label>
-                        <Picker 
-                            placeholder='Compra | Venda'
-                            iosIcon={<Icon name="ios-arrow-down-outline" />}
-                            mode='dropdown'
-                            style={{  }}
-                            placeholderStyle={{color:'#E65100'}}
-                            selectedValue={this.state.operation}
-                            onValueChange={this.onOperationChange.bind(this)}>
+                        <Item inlineLabel style={styles.item}>
+                            <Label style={styles.label}>Operação</Label>
+                            <Picker 
+                                iosHeader='Operação'
+                                placeholder='Compra | Venda'
+                                iosIcon={<Icon name="ios-arrow-down-outline" />}
+                                mode='dropdown'
+                                style={{  }}
+                                placeholderStyle={{color:'#E65100'}}
+                                selectedValue={this.state.operation}
+                                onValueChange={this.onOperationChange.bind(this)}>
 
-                            <Picker.Item key={1} value={'C'} label={'Compra'} />
-                            <Picker.Item key={2} value={'V'} label={'Venda'} />
+                                <Picker.Item key={1} value={'C'} label={'Compra'} />
+                                <Picker.Item key={2} value={'V'} label={'Venda'} />
 
-                        </Picker>
-                    </Item>
+                            </Picker>
+                        </Item>
 
-                    <Item inlineLabel style={styles.item}>
-                        <Label style={styles.label}>Lote</Label>
-                        <Input style={styles.input} 
-                                placeholder='  ...   '
-                                placeholderTextColor={'#E65100'}
-                                keyboardType='numeric'
-                                onChangeText={this.onLoteChange.bind(this)}
-                                />
-                    </Item>
+                        <Item inlineLabel style={styles.item}>
+                            <Label style={styles.label}>Lote</Label>
+                            <Input style={styles.input} 
+                                    placeholder='  ...   '
+                                    placeholderTextColor={'#E65100'}
+                                    keyboardType='numeric'
+                                    onChangeText={this.onLoteChange.bind(this)}
+                                    />
+                        </Item>
 
-                    <Item inlineLabel style={styles.item}>
-                        <Label style={styles.label}>Preço</Label>
-                        <Input style={styles.input} 
-                                placeholder='  $   '
-                                placeholderTextColor={'#E65100'}
-                                keyboardType='numeric'
-                                onChangeText={this.onPriceChange.bind(this)}
-                                />
-                    </Item>
+                        <Item inlineLabel style={styles.item}>
+                            <Label style={styles.label}>Preço</Label>
+                            <Input style={styles.input} 
+                                    placeholder='  $   '
+                                    placeholderTextColor={'#E65100'}
+                                    keyboardType='numeric'
+                                    onChangeText={this.onPriceChange.bind(this)}
+                                    />
+                        </Item>
 
-                    <Item inlineLabel style={styles.item}>
-                        <Label style={styles.label}>Contraparte</Label>
-                        <Input style={styles.input} 
-                                placeholder='Nome do trouxa'
-                                placeholderTextColor={'#E65100'}
-                                keyboardType='default'
-                                onChangeText={this.onContraparteChange.bind(this)}
-                                />
-                    </Item>
+                        <Item inlineLabel style={styles.item}>
+                            <Label style={styles.label}>Contraparte</Label>
+                            <Input style={styles.input} 
+                                    placeholder='Nome do trouxa'
+                                    placeholderTextColor={'#E65100'}
+                                    keyboardType='default'
+                                    onChangeText={this.onContraparteChange.bind(this)}
+                                    />
+                        </Item>
 
-                    <Item inlineLabel style={styles.item}>
-                        <Label style={styles.label}>Vencimento</Label>
-                        <DatePicker
-                            defaultDate={new Date()}
-                            minimumDate={new Date(2000, 1, 1)}
-                            maximumDate={new Date(2100, 12, 31)}
-                            locale={"en"}
-                            timeZoneOffsetInMinutes={undefined}
-                            modalTransparent={true}
-                            animationType={'slide'}
-                            androidMode={"default"}
-                            placeHolderText="Selecione o vencimento"
-                            textStyle={{ color: "grey" }}
-                            placeHolderTextStyle={{ color: "#d3d3d3" }}
-                            onDateChange={this.onSettleDateChange.bind(this)}
-                        />
-                    </Item>
-                    
-                    <Item stackedLabel>
-                        <Label>Comentários</Label>
-                        <Textarea style={styles.textArea}
-                            placeholder='' 
-                            rowSpan={5}
-                            onChangeText={this.onCommentChange.bind(this)}/>
-                    </Item>
+                        <Item inlineLabel style={styles.item}>
+                            <Label style={styles.label}>Vencimento</Label>
+                            <DatePicker
+                                defaultDate={new Date()}
+                                minimumDate={new Date(2000, 1, 1)}
+                                maximumDate={new Date(2100, 12, 31)}
+                                locale={"en"}
+                                timeZoneOffsetInMinutes={undefined}
+                                modalTransparent={true}
+                                animationType={'slide'}
+                                androidMode={"default"}
+                                placeHolderText="Selecione o vencimento"
+                                textStyle={{ color: "grey" }}
+                                placeHolderTextStyle={{ color: "#d3d3d3" }}
+                                onDateChange={this.onSettleDateChange.bind(this)}
+                            />
+                        </Item>
+                        
+                        <Item stackedLabel>
+                            <Label>Comentários</Label>
+                            <Textarea style={styles.textArea}
+                                placeholder='' 
+                                rowSpan={5}
+                                onChangeText={this.onCommentChange.bind(this)}/>
+                        </Item>
 
-                    
-            
+                        
+                
 
-                    {/* <Text>Ativo: {this.state.ativo}</Text>
-                    <Text>Operation: {this.state.operation}</Text>
-                    <Text>Lote: {this.state.lote}</Text>
-                    <Text>Price: {this.state.tradePrice}</Text>
-                    <Text>Contraparte: {this.state.contraparte}</Text>
-                    <Text>Trade Date: {moment(this.state.tradeDate).locale('pt-br').format('D [de] MMM [,] YYYY')}</Text>
-                    <Text>Settle Date: {moment(this.state.settleDate).locale('pt-br').format('D [de] MMM [,] YYYY')}</Text>
-                    <Text>Comentarios: {this.state.comments}</Text> */}
-                </Form>
+                        {/* <Text>Ativo: {this.state.ativo}</Text>
+                        <Text>Operation: {this.state.operation}</Text>
+                        <Text>Lote: {this.state.lote}</Text>
+                        <Text>Price: {this.state.tradePrice}</Text>
+                        <Text>Contraparte: {this.state.contraparte}</Text>
+                        <Text>Trade Date: {moment(this.state.tradeDate).locale('pt-br').format('D [de] MMM [,] YYYY')}</Text>
+                        <Text>Settle Date: {moment(this.state.settleDate).locale('pt-br').format('D [de] MMM [,] YYYY')}</Text>
+                        <Text>Comentarios: {this.state.comments}</Text> */}
+                    </Form>
+                </KeyboardAwareScrollView>
 
             </Container>
 
--- src/screens/Wallet.js
+++ src/screens/Wallet.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react'
-import { View , FlatList, StyleSheet} from 'react-native'
+import { FlatList, StyleSheet } from 'react-native'
 import WalletHeader from '../components/WalletHeader'
 import { Container,
-    Header,
-    Content, 
-    ListItem,
-    Left,
-    Body, Right, Thumbnail, Text ,
-    Button,
-    Icon,
-    SwipeRow,
+    Content,
 } from 'native-base'
 import BoletaItem from '../components/BoletaItem'
 import moment from 'moment'
@@ -20,15 +13,15 @@ const boletas = [
     {id: 1, checked:true, ativo:'Alemanha', operation: 'C', contraparte: 'Fill', lote: 100, tradePrice: 19, tradeDate: '15/09/2018'},
     {id: 2, checked:false, ativo:'Brasil', operation: 'V', contraparte: 'Marcus', lote: 100, tradePrice: 25, tradeDate: '01/06/2013'},
     {id: 3, checked:false, ativo:'França', operation: 'V', contraparte: 'Naman', lote: 50, tradePrice: 13, tradeDate: '01'},
-    {id: 4, checked:true, ativo:'Alemanha', operation: 'C', contraparte: 'Sudano', lote: 20, tradePrice: 22, tradeDate: new Date()},
-    {id: 5, checked:false, ativo:'Belgica', operation: 'V', contraparte: 'Artur Lida', lote: 30, tradePrice: 9, tradeDate: new Date()},
-    {id: 6, checked:true, ativo:'Argentina', operation: 'V', contraparte: 'Caetano Ramos', lote: 75, tradePrice: 7, tradeDate: new Date()},              
-    {id: 7, checked:true, ativo:'Alemanha', operation: 'C', contraparte: 'Fill', lote: 100, tradePrice: 19, tradeDate: '15/09/2018'},
-    {id: 8, checked:false, ativo:'Brasil', operation: 'C', contraparte: 'Marcus', lote: 100, tradePrice: 25, tradeDate: '01/06/2013'},
-    {id: 9, checked:false, ativo:'França', operation: 'C', contraparte: 'Naman', lote: 50, tradePrice: 13, tradeDate: '01'},
-    {id: 10, checked:true, ativo:'Alemanha', operation: 'C', contraparte: 'Sudano', lote: 20, tradePrice: 22, tradeDate: new Date()},
-    {id: 11, checked:false, ativo:'Belgica', operation: 'V', contraparte: 'Artur Lida', lote: 30, tradePrice: 9, tradeDate: new Date()},
-    {id: 12, checked:true, ativo:'Argentina', operation: 'V', contraparte: 'Caetano Ramos', lote: 75, tradePrice: 7, tradeDate: new Date()},              
+    // {id: 4, checked:true, ativo:'Alemanha', operation: 'C', contraparte: 'Sudano', lote: 20, tradePrice: 22, tradeDate: new Date()},
+    // {id: 5, checked:false, ativo:'Belgica', operation: 'V', contraparte: 'Artur Lida', lote: 30, tradePrice: 9, tradeDate: new Date()},
+    // {id: 6, checked:true, ativo:'Argentina', operation: 'V', contraparte: 'Caetano Ramos', lote: 75, tradePrice: 7, tradeDate: new Date()},              
+    // {id: 7, checked:true, ativo:'Alemanha', operation: 'C', contraparte: 'Fill', lote: 100, tradePrice: 19, tradeDate: '15/09/2018'},
+    // {id: 8, checked:false, ativo:'Brasil', operation: 'C', contraparte: 'Marcus', lote: 100, tradePrice: 25, tradeDate: '01/06/2013'},
+    // {id: 9, checked:false, ativo:'França', operation: 'C', contraparte: 'Naman', lote: 50, tradePrice: 13, tradeDate: '01'},
+    // {id: 10, checked:true, ativo:'Alemanha', operation: 'C', contraparte: 'Sudano', lote: 20, tradePrice: 22, tradeDate: new Date()},
+    // {id: 11, checked:false, ativo:'Belgica', operation: 'V', contraparte: 'Artur Lida', lote: 30, tradePrice: 9, tradeDate: new Date()},
+    // {id: 12, checked:true, ativo:'Argentina', operation: 'V', contraparte: 'Caetano Ramos', lote: 75, tradePrice: 7, tradeDate: new Date()},              
 ]
 
 export default class Wallet extends Component {
@@ -39,22 +32,41 @@ export default class Wallet extends Component {
     constructor(props){
         super(props)
         this.state = {
+            resultadosPosiveis: ['Brasil','Argentina','França','Alemanha'],
             bookTitle:'Copa 2018',
             boletas: boletas,
             boletasVisiveis: [],
             showAddBoleta: false,
-            filtroChecked: false
+            filtroChecked: false,
         }
+    }
+    
+    newBoletaAdd = (newBoleta) => {
+        // const newBoleta = this.props.navigation.getParam('newBoleta')
+        const boletas_id = []
+        const boletas = [...this.state.boletas]
+
+        console.log(`NewBoleta:${newBoleta}`)
+
+        boletas.forEach((boleta) => {
+            boletas_id.push(boleta.id)
+        })
+        console.log(boletas_id)
+        if(boletas_id.includes(newBoleta.id)){
+            return
+        } else {
+            boletas.push(newBoleta)
+        }
+
+        this.setState({boletas}, this.filterBoletas)
     }
 
     filterBoletas = () => {
         let oldBoletas = [ ...this.state.boletas ]
         let refresh = !this.state.refresh
         let boletasVisiveis = this.state.filtroChecked ? oldBoletas.filter(item => !item.checked) : oldBoletas
-        console.log(`Filtro checked: ${this.state.filtroChecked}`,boletasVisiveis)
-        console.log(`Boletas Visiveis usadas no setState:`,boletasVisiveis)
        
-        this.setState({boletasVisiveis}, console.log('filtrei', this.state))
+        this.setState({boletasVisiveis})
         
     }
 
@@ -79,7 +91,8 @@ export default class Wallet extends Component {
     }
 
     _openNovaBoleta = () => {
-        this.props.navigation.navigate('BoletaScreen')
+        const resultadosPossiveis = [ ...this.state.resultadosPosiveis]
+        this.props.navigation.navigate('BoletaScreen',{resultadosPossiveis})
     } 
 
     toggleEye = () => {
@@ -96,7 +109,10 @@ export default class Wallet extends Component {
     }
 
     componentDidUpdate() {
-        // this.filterBoletas()
+        const newBoleta = this.props.navigation.getParam('newBoleta')
+        if (newBoleta) {
+            this.newBoletaAdd(newBoleta)
+        }
     }
 
     test = () => {
